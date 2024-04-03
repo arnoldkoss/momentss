2123027db17d352fd1ea28069cb50a071678eb61
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-03 14:51:31 +0000

    start wireing the navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
 import NavBar from './components/NavBar';
 import styles from './App.module.css';
+import Container from "react-bootstrap/Container";
+import { Route, Switch } from "react-router-dom";
 
 function App() {
   return (
     <div className={styles.App}>
       
       <NavBar />
+      <Container className={styles.Main}>
+        <Switch>
+          <Route exact path="/" render={() => <h1>Home page</h1>} />
+          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
+        </Switch>
+      </Container>
     </div>
   );
 }
