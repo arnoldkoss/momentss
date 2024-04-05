4760dd4db0fda897fb05c55cb7497462e4dc00c5
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-05 18:25:05 +0000

    start the signin form

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Container from "react-bootstrap/Container";
 import { Route, Switch } from "react-router-dom";
 import './api/axiosDefaults'
 import SignUpForm from './pages/auth/SignUpForm';
+import SignInForm from './pages/auth/SignInForm';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route exact path="/" render={() => <h1>Home page</h1>} />
           <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
           <Route exact path="/signup" render={() =><SignUpForm />} />
+          <Route exact path="/signin" render={() =><SignInForm />} />
           <Route render={() => <p>Page not found!</p>} />
         </Switch>
       </Container>
