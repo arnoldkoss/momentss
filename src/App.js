2a200bf73569f92710e378a87439c96201df11e4
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-06 14:21:57 +0000

    create the signin form

--- src/App.js
+++ src/App.js
@@ -15,7 +15,6 @@ function App() {
       <Container className={styles.Main}>
         <Switch>
           <Route exact path="/" render={() => <h1>Home page</h1>} />
-          <Route exact path="/signin" render={() => <h1>Sign In</h1>} />
           <Route exact path="/signup" render={() =><SignUpForm />} />
           <Route exact path="/signin" render={() =><SignInForm />} />
           <Route render={() => <p>Page not found!</p>} />
