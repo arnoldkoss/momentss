825a71a114edf81b078f25af93f5ec92703a0a51
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-07 11:17:08 +0000

    create the logged in navbar elements

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,8 @@
 import axios from "axios";
-import { TabContent } from "react-bootstrap";
 
-axios.defaults.baseURL = 'https://drf-appi-ecb8a38b4305.herokuapp.com/'
-axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
-axios.defaults.withCredentials = true
+axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
+axios.defaults.withCredentials = true;
+
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
