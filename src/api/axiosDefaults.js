70fe0e7ae3213535f4c9073f900af5898e8a010e
arnold buiuca <arnoldbuiuka@gmail.com>
2024-04-07 13:54:27 +0000

    get a custom hook to the navbar toggle burger

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "https://drf-api-rec.herokuapp.com/";
+axios.defaults.baseURL = "https://drf-appi-ecb8a38b4305.herokuapp.com/";
 axios.defaults.headers.post["Content-Type"] = "multipart/form-data";
 axios.defaults.withCredentials = true;
 
